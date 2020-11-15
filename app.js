8edc0b614e2afcfd2263e52ea88aa8ca94efc4e2
kimjane93 <kjchadwell93@gmail.com>
2020-11-15 15:50:07 -0500

    troubleshoot isWinner logic

--- app.js
+++ app.js
@@ -3,10 +3,8 @@
 
 const colors = {
     'null': null,
-    '1': 'blue',
-    '-1': 'red',
     '1': 'B',
-    '-1': 'T'
+    '-1': 'T',
 }
 
 // make 1 and -1 an object, and have key value pairs of color, and name
@@ -67,6 +65,7 @@ function init() {
     board = [null, null, null, null, null, null, null, null]
     turn = 1
     winner = null
+    document.getElementById('winner-image').innerHTML = ''
     render()
 }
 
@@ -83,9 +82,12 @@ for (let i = 0; i < board.length+1; i++) {
         }
         else{
             board[Number(e.target.id)] = turn
+            console.log(board)
             turn *= -1
-            winner = isWinner()
+            isWinner()
+            console.log(`winner ${winner}`)
             render()
+            
         }
     }
 }
@@ -96,51 +98,69 @@ for (let i = 0; i < board.length+1; i++) {
 // a winner and changes the state of the winner
 // variable if so
 
+
 // function isWinner() {
 //     for(let i = 0; i < possibleWinScenarios.length; i++){
-//         let singleCombo = possibleWinScenarios[i]
-//         if(singleCombo.every(function (num) {
-//             if(board.includes(num)){
-//                 return true
-//             }
+//         if(Math.abs(board[possibleWinScenarios[i][0]] + board[possibleWinScenarios[i][1]] + board[possibleWinScenarios[i][2]] === 3)){
+//             return board[possibleWinScenarios[i][0]]
 //         }
-//         return board[possibleWinScenarios[i]]
-
-function isWinner() {
-    for(let i = 0; i < possibleWinScenarios.length; i++){
-        if(Math.abs(board[possibleWinScenarios[i][0]] + board[possibleWinScenarios[i][1]] + board[possibleWinScenarios[i][2]] === 3)){
-            return board[possibleWinScenarios[i][0]]
-        }
-    }
-    if(board.includes(null)){
-        return null
-    }
-    else{
-        return 'No One Wins'
-    }
-}
-
-// function isWinner() {
-//     possibleWinScenarios.forEach(function (combo) {
-        
-        
-        // for (let i = 0; i < combo.length; i++) {
-        //     const square = combo[i]
-        //     if(board[square]){
-        //         return board[square]
-        //     }
-        // }
-//     })
+//     }
 //     if(board.includes(null)){
-//         return null
+//        return null
 //     }
 //     else{
 //         return 'No One Wins'
 //     }
 // }
 
+    // function isWinner() {
+    //     for(let i = 0; i < possibleWinScenarios.length; i++){
+    //         if(Math.abs(board[possibleWinScenarios[i][0]] + board[possibleWinScenarios[i][1]] + board[possibleWinScenarios[i][2]] === 3)){
+    //             winner = board[possibleWinScenarios[i][0]]
+    //             return winner
+    //         }
+    //     }
+    //     // if(board.includes(null)){
+    //     //    return null
+    //     // }
+    //     // else{
+    //     //     return 'No One Wins'
+    //     // }
+    // }
+
+function isWinner(){
+    possibleWinScenarios.forEach(function (combo) {
+        if(board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]){
+            winner = board[combo[0]]
+        }
+    })
+    return winner
+}
 
 // Render function:
 // Displays the current state of the board
 // on the page, updating the elements to reflect
 // either X or O depending on whose turn it is
+function render() {
+    board.forEach(function (indSq, idx) {
+        theBoard[idx].style.background = colors[indSq]
+        theBoard[idx].innerHTML = colors[indSq]
+    })
+    if(winner === 'No One Wins'){
+        document.getElementById('winner-image').innerHTML = "<img src='https://www.nhpr.org/sites/nhpr/files/styles/x_large/public/202010/CoronavirusBall_red_CDChighrez.png' width='300' height ='200'>"
+        proclaimer.innerHTML = 'We All Lose, Covid Wins, Better Luck in 2021'
+    }
+    else if(winner){
+        if(winner == 1){
+            document.getElementById('winner-image').innerHTML = "<img src='https://deadline.com/wp-content/uploads/2019/10/shutterstock_editorial_10434333bm.jpg' width='300' height='200'>"
+            proclaimer.innerHTML = `Whoop-dee-doo ${colors[winner].toUpperCase()} Wins!`
+        }
+    else{
+        document.getElementById('winner-image').innerHTML = "<img src='https://media4.s-nbcnews.com/j/streams/2014/December/141208/1D274907376164-today-sad-biden-141208-tease.social_share_1024x512_center.jpg' width='300' height='200'>"
+         proclaimer.innerHTML = `${colors[winner].toUpperCase()} Wins, Stop The Count!`
+    }
+}
+    else{
+        proclaimer.innerHTML = `${colors[turn].toUpperCase()}'s Turn`
+    }
+}
