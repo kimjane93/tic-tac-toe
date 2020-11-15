cb91110f0e613e2d59f08d211fa02e80de56e6d9
kimjane93 <kjchadwell93@gmail.com>
2020-11-14 20:53:35 -0500

    add event listeners, preliminary css, toubleshoot  onClick and init functions

--- app.js
+++ app.js
@@ -1,44 +1,34 @@
 /*-------------------------------- Constants --------------------------------*/
-let covidWins = unclickedSpaces < 1
+// let noOneWins = unclickedSpaces < 1
+
 const colors = {
-    null: ,
-    player1: 1,
-    player2: -1,
+    'null': null,
+    '1': 'blue',
+    '-1': 'red',
+    '1': 'Biden',
+    '-1': 'Trump'
 }
 
-let win1 = [sq1, sq2, sq3]
-let win1 = [sq1, sq4, sq7]
-let win1 = [sq1, sq5, sq9]
-let win1 = [sq2, sq5, sq8]
-let win1 = [sq3, sq6, sq9]
-let win1 = [sq4, sq5, sq6]
-let win1 = [sq7, sq8, sq9]
-let win1 = [sq7, sq5, sq3]
+// make 1 and -1 an object, and have key value pairs of color, and name
+
+const possibleWinScenarios = [
+[0, 1, 2],
+[0, 3, 6],
+[0, 4, 8],
+[1, 4, 7],
+[2, 5, 8],
+[3, 4, 5],
+[6, 7, 8],
+[6, 4, 2]
+]
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+
 // Variables might include (board/turn/winner)
-const players = {
-    player1: {
-        name: 'Biden',
-        gamesWon: null,
-    },
-    player2: {
-        name: 'Trump',
-        gamesWon: null,
-    }
-}
 
-const board = {
-    unclickedSpaces: 9,
-    Player1Spaces: null,
-    Player2Spaces: null,
-}
-
-const board = [[sq1, sq2, sq3],
-                [sq4, sq5, sq6],
-                [sq7, sq8, sq9 ]]
+let turn, winner, board
 
 
 
@@ -47,20 +37,26 @@ const board = [[sq1, sq2, sq3],
 // You might choose to put your game status here
 
 
+const theBoard = document.querySelectorAll('.board div')
+
+const proclaimer = document.querySelector('#message')
+
+
 /*----------------------------- Event Listeners -----------------------------*/
 
 // This is where you should put the event listener
 // for a mouse-click
-document.body.addEventListener('click', (e) => {
-    if(e.target.id === ){
-        // change color/add message to box
-        // if it's still empty and its the appropriate turn
-    }
-    // console.log(e.target.id) if you click button will log its id/file being called by event triggering the event listener
+document.querySelector('.board').addEventListener('click', onClick)
+document.querySelector('#button').addEventListener('click', init)
+document.querySelector('#music-button').addEventListener('click', function (e) {
+    let moodMusic = document.getElementById('mood-music')
+    moodMusic.play()
 })
 
-/*-------------------------------- Functions --------------------------------*/
 
+    // console.log(e.target.id) if you click button will log its id/file being called by event triggering the event listener
+/*-------------------------------- Functions --------------------------------*/
+init()
 
 // Some functions you might choose to use:
 
@@ -68,35 +64,44 @@ document.body.addEventListener('click', (e) => {
 // Where you set your initial state, setting up 
 // what the board will look like upon loading
 function init() {
-    this.play()
-    this.render()
+    board = [null, null, null, null, null, null, null, null]
+    turn = 1
+    winner = null
+    render()
 }
-
 
 
 // On-Click function:
 // Set up what happens when one of the elements
 // is clicked
 
-function onClick() {
-    
+function onClick(e) {
+for (let i = 0; i < board.length+1; i++) {
+    if(Number(e.target.id) === i){
+        if(board[i] || winner){
+            return
+        }
+        else{
+            board[Number(e.target.id)] = turn
+            turn *= -1
+            winner = isWinner()
+            render()
+        }
+    }
 }
-
+}
 
 // Check winner function:
 // Checks the current state of the board for
 // a winner and changes the state of the winner
 // variable if so
 
-function isWinner() {
-    
-}
-
-// Render function:
-// Displays the current state of the board
-// on the page, updating the elements to reflect
-// either X or O depending on whose turn it is
-
-function render() {
-    
-}
+// function isWinner() {
+//     for(let i = 0; i < possibleWinScenarios.length; i++){
+//         let singleCombo = possibleWinScenarios[i]
+//         if(singleCombo.every(function (num) {
+//             if(board.includes(num)){
+//                 return true
+//             }
+//         }
+//         return board[possibleWinScenarios[i]]
